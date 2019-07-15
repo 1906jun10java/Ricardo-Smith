344f5b7c4c16cccd47913a6f47e2f3bd84f79a32
redelization <ricardosmithco@gmail.com>
2019-07-15 14:22:53 -0400

    adding coding chalenge for bookshelf

---
+++ Bookshelf.js
@@ -0,0 +1,57 @@
+var bookList = [{name: "Book1", author: "Author 1", status: "read"},
+                {name: "Book2", author: "Author2", status: "unread"},
+                {name: "Book3", author: "Author3", status: "reading"}];
+
+
+
+
+
+
+function biswas(){
+    console.log('This is a book by famous Trinidadian author VSN!');
+    console.log('Naipaul won the Nobel Peace Price for this book closely based on the life of his father.')
+}
+
+function dragon(){
+    console.log('This is a book by famous Trinidadian author Earl Lovelace!');
+    console.log('LOvelace captured the spirit of Carnival in this timeless novel.')
+}
+
+
+
+
+
+/*
+
+//the following block of code draws the shelf
+fill(173, 117, 33);
+rect(0, 120, width, 10);
+
+
+//the following block of code draws one book:
+fill(214, 255, 219);
+rect(10, 20, 90, 100);
+fill(0, 0, 0);
+text(bookList.name, 15, 29, 70, 100);
+
+for (var i=0; i<bookList.length; i++){
+
+    fill(214, 255, 219);
+    rect(10, 20, 90, 100);
+    fill(0, 0, 0);
+    text(bookList[i].name, 15, 29, 70, 100);
+}
+
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
